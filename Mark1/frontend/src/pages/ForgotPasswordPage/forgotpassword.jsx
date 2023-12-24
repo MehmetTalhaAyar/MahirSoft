14589655b51e642225fece5e2e028bf707811862
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2023-12-24 16:28:17 +0300

    Forgot password page successfully

--- Mark1/frontend/src/pages/ForgotPasswordPage/forgotpassword.jsx
+++ Mark1/frontend/src/pages/ForgotPasswordPage/forgotpassword.jsx
@@ -1,10 +1,31 @@
+import '../ForgotPasswordPage/forgotpassword.css'
 
+export function ForgotPassword() {
+    return (
+        <div className="password_page">
+            <div className='container'>
+                <h1 className='title-field'>
+                    Forgot Password
+                </h1>
 
-export function ForgotPassword(){
-    return <>
-    <div>
-        Forgot Passord Page
-    </div>
-    </>
+                <div className='paragraf-field'>
+                    <p>
+                        Reset your password
+                    </p>
+                </div>
+
+                <div className='email-field'>
+                    <input id="email" required className='email-field-value' />
+                    <label htmlFor='email'>Email</label>
+                </div>
+
+                <div className='continue-buttton-field'>
+                    <button className='continue-buttton-field-value'>
+                        Continue
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
