a3820d6b22430b5a8f1b9ffe05f6e9ccdabea709
redon <redoncapuni@gmail.com>
2023-12-24 13:05:37 +0300

    Update tasks by adding 1 to the value of id for each existing task

--- Mark1/frontend/src/pages/HomePage/DashboardPage/stage.jsx
+++ Mark1/frontend/src/pages/HomePage/DashboardPage/stage.jsx
@@ -1,15 +1,38 @@
 import "./stage.css";
 import Task from "./task";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Stage() {
   const [tasks, setTasks] = useState([]);
 
+  useEffect(() => {
+    // Add a class to trigger the slide-down animation when tasks change
+    const scrollingVertically = document.querySelector(".scrolling_vertically");
+    scrollingVertically.classList.add("slide-down");
+
+    const animationDuration = 500; // Adjust this value based on your CSS animation duration
+    setTimeout(() => {
+      scrollingVertically.classList.remove("slide-down");
+    }, animationDuration);
+  }, [tasks]);
+
   const handleNewButtonClick = () => {
     // Add a new task to the tasks array
-    setTasks([...tasks, <Task key={tasks.length} taskName="Redon" />]);
-  };
+    const newTask = {
+      id: 0,
+      taskName: "Redon",
+    };
 
+    const updatedTasks = tasks.map((task) => ({
+      ...task,
+      id: task.id + 1,
+    }));
+
+    console.log("New Task:", newTask);
+    console.log("All Tasks Before:", tasks);
+    setTasks([newTask, ...updatedTasks]);
+    console.log("All Tasks After:", [newTask, ...updatedTasks]);
+  };
   return (
     <div className="scrolling">
       <ul className="cards">
@@ -21,7 +44,11 @@ function Stage() {
             </button>
           </div>
 
-          <div className="scrolling_vertically">{tasks}</div>
+          <div className="scrolling_vertically">
+            {tasks.map((task) => (
+              <Task key={task.id} taskName={task.taskName} />
+            ))}
+          </div>
         </li>
       </ul>
     </div>
