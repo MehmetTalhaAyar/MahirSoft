72e041703ae94f2d893993c91a42eb42d98fcc19
redon <redoncapuni@gmail.com>
2024-01-01 13:54:47 +0300

    Creating the Dashboard Page and Project Page

--- Mark1/frontend/src/pages/HomePage/DashboardPage/board.jsx
+++ Mark1/frontend/src/pages/HomePage/DashboardPage/board.jsx
@@ -1,4 +1,41 @@
+import "./board.css";
+
 function Board() {
-  return <h1>Dashboard Page</h1>;
+  return (
+    <main>
+      <h1>Dashboard Page</h1>
+      <div className="dashboard_container">
+        <h1 className="dashboard_title">Hi, Ali Duru</h1>
+        <p className="dashboard_paragraph">
+          Check out any upcoming tasks and recent projects below{" "}
+        </p>
+
+        {/*<img src="/src/assets/images.jpg" className="dashboard_img" /> */}
+      </div>
+      <div className="dashboard_cards">
+        <div className="dashboard_task">
+          <div className="total_tasks">
+            <h1>3</h1>
+            <span>Total Task</span>
+          </div>
+          <img src="/src/assets/images.jpg" className="dashboard_img" />
+        </div>
+        <div className="dashboard_task">
+          <div className="total_tasks">
+            <h1>2</h1>
+            <span>Pending</span>
+          </div>
+          <img src="/src/assets/images.jpg" className="dashboard_img" />
+        </div>
+        <div className="dashboard_task">
+          <div className="total_tasks">
+            <h1>1</h1>
+            <span>Due Today</span>
+          </div>
+          <img src="/src/assets/images.jpg" className="dashboard_img" />
+        </div>
+      </div>
+    </main>
+  );
 }
 export default Board;
--- Mark1/frontend/src/pages/HomePage/Project/projects.jsx
+++ Mark1/frontend/src/pages/HomePage/Project/projects.jsx
@@ -1,8 +1,57 @@
+import React, { useState } from "react";
+import "./project.css";
+
+import { HiDotsHorizontal } from "react-icons/hi";
+
 function Project() {
+  // State to track the project cards
+  const [projectCards, setProjectCards] = useState(["Project Title"]);
+  const [isDropdown, setIsOpenDropdown] = useState(false);
+
+  // Function to add a new project card
+  const addProjectCard = () => {
+    const newProjectCards = [...projectCards, "New Project"];
+    setProjectCards(newProjectCards);
+  };
+
+  const openDropdownMenu = () => {
+    setIsOpenDropdown(!isDropdown);
+  };
+
   return (
-    <>
-      <h1>Project Page</h1>
-    </>
+    <main>
+      <h1>My Project</h1>
+      <button className="project_button" onClick={addProjectCard}>
+        New Project
+      </button>
+
+      <div className="project_section">
+        {/* Map through projectCards array to render project cards */}
+        {projectCards.map((title, index) => (
+          <div className="project_card" key={index}>
+            <HiDotsHorizontal className="_dot" onClick={openDropdownMenu} />
+
+            <div
+              id="myDropdown"
+              className={`dropdown-content ${isDropdown ? "openDropdown" : ""}`}
+            >
+              <a href="#delete">Delete</a>
+            </div>
+            <div className="proje_container">
+              <h2 className="proje_title">{title}</h2>
+              <div className="proje_tasks_container">
+                <div className="proje_task">Task</div>
+                <div className="proje_task">Task</div>
+                <div className="proje_task">Task</div>
+              </div>
+            </div>
+            <span className="created_on">Created on January 01</span>
+          </div>
+        ))}
+      </div>
+      {/* Button to add a new project card */}
+    </main>
   );
 }
+
 export default Project;
